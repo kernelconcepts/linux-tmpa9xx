fe8877a06a27805005bef6bc582d8418085a47e5
Florian Boor <florian@kernelconcepts.de>
2009-04-06 17:16:59 +0200

    Some gpiolib improvement.

--- arch/arm/mach-tmpa910/gpio.c
+++ arch/arm/mach-tmpa910/gpio.c
@@ -13,108 +13,111 @@
  * published by the Free Software Foundation.
  */
 
+/* TODO: Interrupt support, check valid modes */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/seq_file.h>
 #include <linux/io.h>
 
-#include <mach/ep93xx-regs.h>
+#include <mach/tmpa910_regs.h>
+#include <mach/gpio.h>
 #include <asm/gpio.h>
 
-struct ep93xx_gpio_chip {
-	struct gpio_chip	chip;
+struct tmpa910_gpio_chip {
+	struct gpio_chip chip;
 
-	unsigned int		data_reg;
-	unsigned int		data_dir_reg;
+	unsigned int data_reg;
+	unsigned int data_dir_reg;
+    
+    unsigned int input_mask;
+    unsigned int output_mask;
 };
 
-#define to_ep93xx_gpio_chip(c) container_of(c, struct ep93xx_gpio_chip, chip)
+#define to_tmpa910_gpio_chip(c) container_of(c, struct tmpa910_gpio_chip, chip)
 
-/* From core.c */
-extern void ep93xx_gpio_int_mask(unsigned line);
-extern void ep93xx_gpio_update_int_params(unsigned port);
-
-static int ep93xx_gpio_direction_input(struct gpio_chip *chip, unsigned offset)
+static int tmpa910_gpio_direction_input(struct gpio_chip *chip, unsigned offset)
 {
-	struct ep93xx_gpio_chip *ep93xx_chip = to_ep93xx_gpio_chip(chip);
+	struct tmpa910_gpio_chip *tmpa910_chip = to_tmpa910_gpio_chip(chip);
 	unsigned long flags;
 	u8 v;
 
 	local_irq_save(flags);
-	v = __raw_readb(ep93xx_chip->data_dir_reg);
+	v = __raw_readb(tmpa910_chip->data_dir_reg);
 	v &= ~(1 << offset);
-	__raw_writeb(v, ep93xx_chip->data_dir_reg);
+	__raw_writeb(v, tmpa910_chip->data_dir_reg);
 	local_irq_restore(flags);
 
 	return 0;
 }
 
-static int ep93xx_gpio_direction_output(struct gpio_chip *chip,
+static int tmpa910_gpio_direction_output(struct gpio_chip *chip,
 					unsigned offset, int val)
 {
-	struct ep93xx_gpio_chip *ep93xx_chip = to_ep93xx_gpio_chip(chip);
+	struct tmpa910_gpio_chip *tmpa910_chip = to_tmpa910_gpio_chip(chip);
 	unsigned long flags;
-	int line;
+//	int line;
 	u8 v;
 
 	local_irq_save(flags);
 
 	/* Set the value */
-	v = __raw_readb(ep93xx_chip->data_reg);
+	v = __raw_readb(tmpa910_chip->data_reg);
 	if (val)
 		v |= (1 << offset);
 	else
 		v &= ~(1 << offset);
-	__raw_writeb(v, ep93xx_chip->data_reg);
+	__raw_writeb(v, tmpa910_chip->data_reg);
 
+#if 0
 	/* Drive as an output */
 	line = chip->base + offset;
-	if (line <= EP93XX_GPIO_LINE_MAX_IRQ) {
+	if (line <= TMPA910_GPIO_LINE_MAX_IRQ) {
 		/* Ports A/B/F */
-		ep93xx_gpio_int_mask(line);
-		ep93xx_gpio_update_int_params(line >> 3);
+		tmpa910_gpio_int_mask(line);
+		tmpa910_gpio_update_int_params(line >> 3);
 	}
-
-	v = __raw_readb(ep93xx_chip->data_dir_reg);
+#endif
+	v = __raw_readb(tmpa910_chip->data_dir_reg);
 	v |= (1 << offset);
-	__raw_writeb(v, ep93xx_chip->data_dir_reg);
+	__raw_writeb(v, tmpa910_chip->data_dir_reg);
 
 	local_irq_restore(flags);
 
 	return 0;
 }
 
-static int ep93xx_gpio_get(struct gpio_chip *chip, unsigned offset)
+static int tmpa910_gpio_get(struct gpio_chip *chip, unsigned offset)
 {
-	struct ep93xx_gpio_chip *ep93xx_chip = to_ep93xx_gpio_chip(chip);
+	struct tmpa910_gpio_chip *tmpa910_chip = to_tmpa910_gpio_chip(chip);
 
-	return !!(__raw_readb(ep93xx_chip->data_reg) & (1 << offset));
+	return !!(__raw_readb(tmpa910_chip->data_reg) & (1 << offset));
 }
 
-static void ep93xx_gpio_set(struct gpio_chip *chip, unsigned offset, int val)
+static void tmpa910_gpio_set(struct gpio_chip *chip, unsigned offset, int val)
 {
-	struct ep93xx_gpio_chip *ep93xx_chip = to_ep93xx_gpio_chip(chip);
+	struct tmpa910_gpio_chip *tmpa910_chip = to_tmpa910_gpio_chip(chip);
 	unsigned long flags;
 	u8 v;
 
 	local_irq_save(flags);
-	v = __raw_readb(ep93xx_chip->data_reg);
+	v = __raw_readb(tmpa910_chip->data_reg);
 	if (val)
 		v |= (1 << offset);
 	else
 		v &= ~(1 << offset);
-	__raw_writeb(v, ep93xx_chip->data_reg);
+	__raw_writeb(v, tmpa910_chip->data_reg);
 	local_irq_restore(flags);
 }
 
-static void ep93xx_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
+static void tmpa910_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 {
-	struct ep93xx_gpio_chip *ep93xx_chip = to_ep93xx_gpio_chip(chip);
+	struct tmpa910_gpio_chip *tmpa910_chip = to_tmpa910_gpio_chip(chip);
 	u8 data_reg, data_dir_reg;
 	int i;
 
-	data_reg = __raw_readb(ep93xx_chip->data_reg);
-	data_dir_reg = __raw_readb(ep93xx_chip->data_dir_reg);
+	data_reg = __raw_readb(tmpa910_chip->data_reg);
+	data_dir_reg = __raw_readb(tmpa910_chip->data_dir_reg);
 
 	for (i = 0; i < chip->ngpio; i++)
 		seq_printf(s, "GPIO %s%d: %s %s\n", chip->label, i,
@@ -122,37 +125,47 @@ static void ep93xx_gpio_dbg_show(struct seq_file *s, struct gpio_chip *chip)
 			   (data_dir_reg & (1 << i)) ? "out" : "in");
 }
 
-#define EP93XX_GPIO_BANK(name, dr, ddr, base_gpio)			\
+#define TMPA910_GPIO_BANK(name, port_base, num, base_gpio)	\
 	{								\
 		.chip = {						\
 			.label		  = name,			\
-			.direction_input  = ep93xx_gpio_direction_input, \
-			.direction_output = ep93xx_gpio_direction_output, \
-			.get		  = ep93xx_gpio_get,		\
-			.set		  = ep93xx_gpio_set,		\
-			.dbg_show	  = ep93xx_gpio_dbg_show,	\
+			.direction_input  = tmpa910_gpio_direction_input, \
+			.direction_output = tmpa910_gpio_direction_output, \
+			.get		  = tmpa910_gpio_get,		\
+			.set		  = tmpa910_gpio_set,		\
+			.dbg_show	  = tmpa910_gpio_dbg_show,	\
 			.base		  = base_gpio,			\
-			.ngpio		  = 8,				\
+			.ngpio		  = num,				\
 		},							\
-		.data_reg	= EP93XX_GPIO_REG(dr),			\
-		.data_dir_reg	= EP93XX_GPIO_REG(ddr),			\
+		.data_reg	= TMPA910_GPIO_REG(port_base, PORT_OFS_DATA),	\
+		.data_dir_reg	= TMPA910_GPIO_REG(port_base, PORT_OFS_DIR),\
 	}
 
-static struct ep93xx_gpio_chip ep93xx_gpio_banks[] = {
-	EP93XX_GPIO_BANK("A", 0x00, 0x10, 0),
-	EP93XX_GPIO_BANK("B", 0x04, 0x14, 8),
-	EP93XX_GPIO_BANK("C", 0x08, 0x18, 40),
-	EP93XX_GPIO_BANK("D", 0x0c, 0x1c, 24),
-	EP93XX_GPIO_BANK("E", 0x20, 0x24, 32),
-	EP93XX_GPIO_BANK("F", 0x30, 0x34, 16),
-	EP93XX_GPIO_BANK("G", 0x38, 0x3c, 48),
-	EP93XX_GPIO_BANK("H", 0x40, 0x44, 56),
+static struct tmpa910_gpio_chip tmpa910_gpio_banks[] = {
+	TMPA910_GPIO_BANK("A", 0x0000, 8, 0),
+	TMPA910_GPIO_BANK("B", 0x1000, 8, 8),
+	TMPA910_GPIO_BANK("C", 0x2000, 8, 16),
+	TMPA910_GPIO_BANK("D", 0x3000, 8, 24),
+	TMPA910_GPIO_BANK("E", 0x4000, 8, 32),
+	TMPA910_GPIO_BANK("F", 0x5000, 8, 40),
+	TMPA910_GPIO_BANK("G", 0x6000, 8, 48),
+	TMPA910_GPIO_BANK("H", 0x7000, 8, 56),
+	TMPA910_GPIO_BANK("J", 0x8000, 8, 72),
+	TMPA910_GPIO_BANK("K", 0x9000, 8, 80),
+	TMPA910_GPIO_BANK("L", 0xA000, 8, 88),
+	TMPA910_GPIO_BANK("M", 0xB000, 8, 96),
+	TMPA910_GPIO_BANK("N", 0xC000, 8, 104),
+	TMPA910_GPIO_BANK("P", 0xD000, 8, 112),
+	TMPA910_GPIO_BANK("R", 0xE000, 8, 120),
+	TMPA910_GPIO_BANK("T", 0xF000, 8, 128),
 };
 
-void __init ep93xx_gpio_init(void)
+void __init tmpa910_gpio_init(void)
 {
 	int i;
 
-	for (i = 0; i < ARRAY_SIZE(ep93xx_gpio_banks); i++)
-		gpiochip_add(&ep93xx_gpio_banks[i].chip);
+	for (i = 0; i < ARRAY_SIZE(tmpa910_gpio_banks); i++)
+		gpiochip_add(&tmpa910_gpio_banks[i].chip);
 }
+
+arch_initcall(tmpa910_gpio_init);
