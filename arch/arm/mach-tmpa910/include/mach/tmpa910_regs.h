9ba5233302b9dd40e6a9f1a20e123976ebef738b
Florian Boor <florian@kernelconcepts.de>
2009-04-07 16:25:40 +0200

    Add NAND flash cotroller register definitions, some comments and unify formatting.

--- arch/arm/mach-tmpa910/include/mach/tmpa910_regs.h
+++ arch/arm/mach-tmpa910/include/mach/tmpa910_regs.h
@@ -22,17 +22,7 @@
 #ifndef __TMPA910_REGS__
 #define __TMPA910_REGS__
 
-/* GPIO PORTS */
-
-/*
- * Base address = 0xF0805000
- * Register    Address
- *                                                       Description
- * Name        (base+)
- * GPIOFDATA   0x03FC      PortF Data Regsiter
- * GPIOFDIR    0x0400      PortF Data Direction Register
- * GPIOFFR1    0x0424      PortF Function Register1
-*/
+/* GPIO Ports */
 
 #define PORT_BASE          0xF0800000
 #define PORTA    0x0000
@@ -98,43 +88,63 @@
 
 
 
-/********/
+/* Timer */
 #define TMPA910_TIMER0 0xf0040000
-/********/
+
+/* LCD Controller */
 #define LCDC_BASE 0xf4200000
-/********/
+
+
+/* I2C Ports */
 #define I2C0_BASE   0xF0070000
 #define I2C1_BASE   0xF0071000
 
-/********/
+/* Camera sensor controller */
 #define CMOSCAM_BASE  0xF2020000
 
-/********/
+/* DMA */
 #define DMAC_BASE  0xF4100000
 
-/********/
+/* System Control */
 #define SYSCTRL_BASE  (0xF0050000)
 
-/********/
-#define NANDF_BASE  0xf2010000
+/* NAND Flash Controller */
+#define NANDF_BASE  0xF2010000
 
-/********/
+#define NDFMCR0  (NANDF_BASE | 0x0000) /* NAND-Flash Control Register 0 */
+#define NDFMCR1  (NANDF_BASE | 0x0004) /* NAND-Flash Control Register 1 */
+#define NDFMCR2  (NANDF_BASE | 0x0008) /* NAND-Flash Control Register 2 */
+#define NDFINTC  (NANDF_BASE | 0x000C) /* NAND-Flash Interrupt Control Register */
+#define NDFDTR   (NANDF_BASE | 0x0010) /* NAND-Flash Data Register */
+#define NDECCRD0 (NANDF_BASE | 0x0020) /* NAND-Flash ECC Read Register 0 */
+#define NDECCRD1 (NANDF_BASE | 0x0024) /* NAND-Flash ECC Read Register 1 */
+#define NDECCRD2 (NANDF_BASE | 0x0028) /* NAND-Flash ECC Read Register 2 */
+#define NDRSCA0  (NANDF_BASE | 0x0030) /* NAND-Flash Reed-Solomon Calculation Result Address Register 0 */
+#define NDRSCD0  (NANDF_BASE | 0x0034) /* NAND-Flash Reed-Solomon Calculation Result Data Register 0 */
+#define NDRSCA1  (NANDF_BASE | 0x0038) /* NAND-Flash Reed-Solomon Calculation Result Address Register 1 */
+#define NDRSCD1  (NANDF_BASE | 0x003C) /* NAND-Flash Reed-Solomon Calculation Result Data Register 1 */
+#define NDRSCA2  (NANDF_BASE | 0x0040) /* NAND-Flash Reed-Solomon Calculation Result Address Register 2 */
+#define NDRSCD2  (NANDF_BASE | 0x0044) /* NAND-Flash Reed-Solomon Calculation Result Data Register 2 */
+#define NDRSCA3  (NANDF_BASE | 0x0048) /* NAND-Flash Reed-Solomon Calculation Result Address Register 3 */
+#define NDRSCD3  (NANDF_BASE | 0x004C) /* NAND-Flash Reed-Solomon Calculation Result Data Register 3 */
+
+
+
+/* LCDDA (LCD Data Process Accelerator) */
 #define LCDDA_BASE  0xF2050000
 
-/********/
+/* Interrupt Controller */
 #define INTR_BASE  0xF4000000
 
-/********/
+/* Touchscreen Controller */
 #define TS_BASE   0xf00601f0
+#define TOUCHSCREEN_BASE   0xF00601F0
 
-/********/
+/* ADC */
 #define ADC_BASE  0xf0080000
-/********/
-#define TOUCHSCREEN_BASE   0xf00601f0
-#define ADC_BASE           0xf0080000
 
-/********/
+/* SDRAM */
 #define SRAM_BASE      0xF8002000
-#define SRAM_SIZE      0x0000c000
+#define SRAM_SIZE      0x0000C000
 
 #endif /* __TMPA910_REGS__ */
