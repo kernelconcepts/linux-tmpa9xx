be7c88235f4637f198a42e7a2909c6d8e61f2844
Florian Boor <florian@kernelconcepts.de>
2010-05-20 15:36:19 +0200

    Make OOB layout compatible with bootloader.

--- drivers/mtd/nand/tmpa910_nand.c
+++ drivers/mtd/nand/tmpa910_nand.c
@@ -86,36 +86,36 @@ struct tmpa9x0_nand_private tmpa9x0_nand = {
 
 /* NAND OOB layout for different chip types */
 static struct nand_ecclayout nand_oob_hamming_512 = {
-	.eccbytes = 6,
-	.eccpos = {0, 1, 2, 3, 6, 7},
-	.oobfree = {
-		{.offset = 8,
-		 . length = 8}}
+    .eccbytes = 6,
+    .eccpos =  {8, 9, 10, 13, 14, 15},
+    .oobfree = { {2, 6} }
 };
 
 static struct nand_ecclayout nand_oob_hamming_2048 = {
-	.eccbytes = 24,
-	.eccpos = {
-		   40, 41, 42, 43, 44, 45, 46, 47,
-		   48, 49, 50, 51, 52, 53, 54, 55,
-		   56, 57, 58, 59, 60, 61, 62, 63},
-	.oobfree = {
-		{.offset = 2,
-		 .length = 38}}
+    .eccbytes = 24,
+    .eccpos   =  { 8,  9, 10, 13, 14, 15, 24, 25,
+                      26, 29, 30, 31, 40, 41, 42, 45,
+                      46, 47, 56, 57, 58, 61, 62, 63},
+    .oobfree  = {{ 2, 6},
+                     {16, 8},
+                     {32, 8},
+                     {48, 8},
+                     {11, 2},
+                     {27, 2},
+                     {43, 2},
+                     {59, 2}}
 };
 
 static struct nand_ecclayout nand_oob_rs_2048 = {
-	.eccbytes = 40,
-	.eccpos = {
-		24, 25, 26, 27, 28,
-		29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
-		39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-		49, 50, 51, 52, 53, 54, 55, 56, 57, 58,
-		59, 60, 61, 62, 63,
-		},
-	.oobfree = {
-		{.offset = 2, .length = 22, },
-	},
+    .eccbytes = 40,
+    .eccpos   =  { 6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+                      22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                      38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+                      54, 55, 56, 57, 58, 59, 60, 61, 62, 63},
+    .oobfree  = {{ 2, 4},
+                     {16, 6},
+                     {32, 6},
+                     {48, 6}}
 };
 
 #ifdef CONFIG_MTD_PARTITIONS
