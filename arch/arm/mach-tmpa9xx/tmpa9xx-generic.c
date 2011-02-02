kernelconcepts/linux-tmpa9xx

commit 2d9ce31b3e3057625d8991d3464c177d77b11722

fix some bit meaning to the correct values

arch/arm/mach-tmpa9xx/tmpa9xx-generic.c
@@ -771,11 +771,11 @@ static void setup_display(void)
 
         tmpa9xx_panel.bpp               =  1 << ((videoparams[3]>>1)&0x07);
 
-        if (!(videoparams[2] & (1<<13)))
+        if (!(videoparams[2] & (1<<11)))
 		sync |= FB_SYNC_HOR_HIGH_ACT;
         if (!(videoparams[2] & (1<<12)))
 		sync |= FB_SYNC_VERT_HIGH_ACT;
-        if (videoparams[2] && (1<<11))
+        if (!(videoparams[2] &&(1<<13)))
 		timer2 |= TIM2_IPC;
 
 	timer2 |= (videoparams[2])&0x1f;

commit 17e6143be5bee61f232471a4c226fb14d1cc2351

fix display bug introduced with 2d9ce31b3e3057625d8991d3464c177d77b11722: TIM2_IPC bit in tim2 register is never checked or touched by clcdfb_decode(), so this bit must be passed as-is. additionally, there was an && vs. & error.

arch/arm/mach-tmpa9xx/tmpa9xx-generic.c
@@ -789,7 +789,7 @@ static void setup_display(void)
 		sync |= FB_SYNC_HOR_HIGH_ACT;
         if (!(videoparams[2] & (1<<12)))
 		sync |= FB_SYNC_VERT_HIGH_ACT;
-        if (!(videoparams[2] &&(1<<13)))
+        if ((videoparams[2] & (1<<13)))
 		timer2 |= TIM2_IPC;
 
 	timer2 |= (videoparams[2])&0x1f;