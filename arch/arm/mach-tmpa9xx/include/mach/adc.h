6a00317c6d70d88ba6137ff8bec5b79fc46085e2
Michael Hunold <michael@mihu.de>
2011-03-02 13:42:49 +0100

    push all adc related handling down into adc driver

--- arch/arm/mach-tmpa9xx/include/mach/adc.h
+++ arch/arm/mach-tmpa9xx/include/mach/adc.h
@@ -1,9 +1,6 @@
 /*
- *  Header file for TMPA9xx ADC Controller
- *
- *  Data structure and register user interface
- *
- *  Copyright (C) 2008 bplan GmbH
+ * ADC functions for TMPA9xx
+ * used by the touchscreen and IIO adc driver
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,42 +18,6 @@
  */
 #ifndef __TMPA9XX_ADC_H__
 #define __TMPA9XX_ADC_H__
-
-struct tmpa9xx_adc
-{
-	uint32_t adreg0l; //   0x0000 a/d conversion result lower-order register 0
-	uint32_t adreg0h; //   0x0004 a/d conversion result higher-order register 0
-	uint32_t adreg1l; //   0x0008 a/d conversion result lower-order register 1
-	uint32_t adreg1h; //   0x000c a/d conversion result higher-order register 1
-	uint32_t adreg2l; //   0x0010 a/d conversion result lower-order register 2
-	uint32_t adreg2h; //   0x0014 a/d conversion result higher-order register 2
-	uint32_t adreg3l; //   0x0018 a/d conversion result lower-order register 3
-	uint32_t adreg3h; //   0x001c a/d conversion result higher-order register 3
-	uint32_t adreg4l; //   0x0020 a/d conversion result lower-order register 4
-	uint32_t adreg4h; //   0x0024 a/d conversion result higher-order register 4
-	uint32_t adreg5l; //   0x0028 a/d conversion result lower-order register 5
-	uint32_t adreg5h; //   0x002c a/d conversion result higher-order register 5
-	uint32_t rsd1;  	//         0x0030 reserved
-	uint32_t rsd2;  	//         0x0034 reserved
-	uint32_t rsd3;  	//         0x0038 reserved
-	uint32_t rsd4;  	//         0x003c reserved
-	uint32_t adregspl; //  0x0040 top-priority a/d conversion result lower-order register
-	uint32_t adregsph; //  0x0044 top-priority a/d conversion result higher-order register
-	uint32_t adcomregl; // 0x0048 a/d conversion result comparison lower-order register
-	uint32_t adcomregh; // 0x004c a/d conversion result comparison lower-order register
-	uint32_t admod0; //    0x0050 a/d mode control register 0
-	uint32_t admod1; //    0x0054 a/d mode control register 1
-	uint32_t admod2; //    0x0058 a/d mode control register 2
-	uint32_t admod3; //    0x005c a/d mode control register 3
-	uint32_t admod4; //    0x0060 a/d mode control register 4
-	uint32_t rsd5;  	//         0x0064 reserved
-	uint32_t rsd6;  	//         0x0068 reserved
-	uint32_t rsd7;  	//         0x006c reserved
-	uint32_t adclk; //     0x0070 a/d conversion clock setting register
-	uint32_t adie; //      0x0074 a/d interrupt enable register
-	uint32_t adis; //      0x0078 a/d interrupt status register
-	uint32_t adic; //      0x007c a/d interrupt clear register
-};
 
 int tmpa9xx_adc_read(int num, int delay);
 
--- arch/arm/mach-tmpa9xx/tmpa9xx-adc.c
+++ arch/arm/mach-tmpa9xx/tmpa9xx-adc.c
@@ -22,15 +22,48 @@
 #include <linux/delay.h>
 #include <linux/io.h>
 
-#include <mach/regs.h>
-#include <mach/adc.h>
-
 #define DRIVER_NAME "tmpa9xx-adc"
+
+#define ADC_ADREG0L   (0x00) /* A/D conversion result lower-order register 0 */
+#define ADC_ADREG0H   (0x04) /* A/D conversion result higher-order register 0 */
+#define ADC_ADREG1L   (0x08) /* A/D conversion result lower-order register 1 */
+#define ADC_ADREG1H   (0x0c) /* A/D conversion result higher-order register 1 */
+#define ADC_ADREG2L   (0x10) /* A/D conversion result lower-order register 2 */
+#define ADC_ADREG2H   (0x14) /* A/D conversion result higher-order register 2 */
+#define ADC_ADREG3L   (0x18) /* A/D conversion result lower-order register 3 */
+#define ADC_ADREG3H   (0x1c) /* A/D conversion result higher-order register 3 */
+#define ADC_ADREG4L   (0x20) /* A/D conversion result lower-order register 4 */
+#define ADC_ADREG4H   (0x24) /* A/D conversion result higher-order register 4 */
+#define ADC_ADREG5L   (0x28) /* A/D conversion result lower-order register 5 */
+#define ADC_ADREG5H   (0x2c) /* A/D conversion result higher-order register 5 */
+#define ADC_ADREG6L   (0x30) /* A/D conversion result lower-order register 6 */
+#define ADC_ADREG6H   (0x34) /* A/D conversion result higher-order register 6 */
+#define ADC_ADREG7L   (0x38) /* A/D conversion result lower-order register 7 */
+#define ADC_ADREG7H   (0x3c) /* A/D conversion result higher-order register 7 */
+#define ADC_ADREGSPL  (0x40) /* Top-priority A/D conversion result lower-order register */
+#define ADC_ADREGSPH  (0x44) /* Top-priority A/D conversion result higher-order register */
+#define ADC_ADCOMREGL (0x48) /* A/D conversion result comparison lower-order register */
+#define ADC_ADCOMREGH (0x4c) /* A/D conversion result comparison lower-order register */
+#define ADC_ADMOD0    (0x50) /* A/D mode control register 0 */
+#define ADC_ADMOD1    (0x54) /* A/D mode control register 1 */
+#define ADC_ADMOD2    (0x58) /* A/D mode control register 2 */
+#define ADC_ADMOD3    (0x5c) /* A/D mode control register 3 */
+#define ADC_ADMOD4    (0x60) /* A/D mode control register 4 */
+#define ADC_ADCLK     (0x70) /* A/D conversion clock setting register */
+#define ADC_ADIE      (0x74) /* A/D interrupt enable register */
+#define ADC_ADIS      (0x78) /* A/D interrupt status register */
+#define ADC_ADIC      (0x7c) /* A/D interrupt clear register */
+
+#define ADC_ADREGxL(x) (x*0x08)
+#define ADC_ADREGxH(x) (x*0x08 + 0x04)
+
+#define adc_writel(x, y, z)	writel(z, x->regs + y)
+#define adc_readl(x, y)		readl(x->regs + y)
 
 struct tmpa9xx_adc_core
 {
 	struct device *dev;
-	volatile struct tmpa9xx_adc *adc;
+	void __iomem *regs;
 	int irq;
 	struct mutex lock;
 	struct completion complete;
@@ -39,12 +72,13 @@ struct tmpa9xx_adc_core
 
 struct tmpa9xx_adc_core *g_h;
 
-/* fixme: this function does not check who is trying to acquire adc 
+/* fixme: this function does not check who is trying to acquire adc
 values. this should probably be handled through some resource handling
 mechanism, which is currently missing. */
 int tmpa9xx_adc_read(int num, int delay)
 {
 	struct tmpa9xx_adc_core *t = g_h;
+	uint32_t val;
 	int ret;
 
 	/* if the global adc core variable is NULL, this
@@ -54,14 +88,18 @@ int tmpa9xx_adc_read(int num, int delay)
 	mutex_lock(&t->lock);
 
 	init_completion(&t->complete);
-	ADC_ADMOD1 = (ADC_ADMOD1 & ~0x0f) | num;
+
+	val = (adc_readl(t, ADC_ADMOD1) & ~0x0f) | num;
+	adc_writel(t, ADC_ADMOD1, val);
 
 	if (delay) {
 		set_current_state(TASK_UNINTERRUPTIBLE);
 		schedule_timeout (msecs_to_jiffies(delay));
 	}
 
- 	ADC_ADMOD0 |= (1 << 0);
+	val = (adc_readl(t, ADC_ADMOD0)) | (1 << 0);
+	adc_writel(t, ADC_ADMOD0, val);
+
 	wait_for_completion(&t->complete);
 	ret = t->value;
 
@@ -70,17 +108,20 @@ int tmpa9xx_adc_read(int num, int delay)
 	return ret;
 }
 
+EXPORT_SYMBOL_GPL(tmpa9xx_adc_read);
+
 static irqreturn_t interrupt_handler(int irq, void *dev)
 {
 	struct tmpa9xx_adc_core *t = dev;
 	int num;
 
 	/* clear interrupt */
-	ADC_ADIC = (1 << 0);
+	adc_writel(t, ADC_ADIC, (1 << 0));
 
-	num = ADC_ADMOD1 & 0x0f;
+	num = adc_readl(t, ADC_ADMOD1) & 0x0f;
 
-	t->value = ((ADC_ADREGxH(num) & 0xff) << 2) | ((ADC_ADREGxL(num) >> 6) & 0x3);
+	t->value = ((adc_readl(t, ADC_ADREGxH(num)) & 0xff) << 2) | ((adc_readl(t, ADC_ADREGxL(num)) >> 6) & 0x3);
+
 	complete(&t->complete);
 
 	return IRQ_HANDLED;
@@ -90,6 +131,7 @@ static int tmpa9xx_adc_probe(struct platform_device *pdev)
 {
 	struct tmpa9xx_adc_core *t;
 	struct resource	*mem;
+	uint32_t val;
 	int ret;
 
 	t = kzalloc(sizeof(*t), GFP_KERNEL);
@@ -107,19 +149,19 @@ static int tmpa9xx_adc_probe(struct platform_device *pdev)
 		goto err0;
 	}
 
-	t->adc = ioremap(mem->start, resource_size(mem));
-	if(!t->adc) {
+	t->regs = ioremap(mem->start, resource_size(mem));
+	if(!t->regs) {
 		dev_err(t->dev, "ioremap() failed\n");
 		goto err1;
 	}
 
 	/* reset */
-	ADC_ADMOD4 = 0x2;
+	adc_writel(t, ADC_ADMOD4, 0x2);
 	udelay(10);
-	ADC_ADMOD4 = 0x1;
+	adc_writel(t, ADC_ADMOD4, 0x1);
 
 	/* disable adc interrupts */
-	ADC_ADIE = 0;
+	adc_writel(t, ADC_ADIE, 0x0);
 
 	t->irq = platform_get_irq(pdev, 0);
 	if (t->irq <= 0) {
@@ -135,13 +177,14 @@ static int tmpa9xx_adc_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, t);
 
-	ADC_ADMOD1 |= 0x80;
+	val = adc_readl(t, ADC_ADMOD1) | 0x80;
+	adc_writel(t, ADC_ADMOD1, 0x80);
 
-        /* Setup ADCCLK */
-        ADC_ADCLK = (0x01 << 7) | 0x02;
+        /* setup ADCCLK */
+        adc_writel(t, ADC_ADCLK, (0x01 << 7) | 0x02);
 
-	/* Enable IRQ generation */
-	ADC_ADIE = (0x01 << 0);
+	/* enable IRQs */
+	adc_writel(t, ADC_ADIE, (0x01 << 0));
 
 	platform_set_drvdata(pdev, t);
 
@@ -153,7 +196,7 @@ static int tmpa9xx_adc_probe(struct platform_device *pdev)
 
 err3:
 err2:
-	iounmap(t->adc);
+	iounmap(t->regs);
 err1:
 err0:
 	kfree(t);
@@ -164,12 +207,12 @@ static int __devexit tmpa9xx_adc_remove(struct platform_device *pdev)
 {
 	struct tmpa9xx_adc_core *t = platform_get_drvdata(pdev);
 
-	/* Disable IRQ generation */
-	ADC_ADIE = 0;
+	/* disable IRQs */
+	adc_writel(t, ADC_ADIE, 0x0);
 
 	free_irq(t->irq, t);
 
-	iounmap(t->adc);
+	iounmap(t->regs);
 
 	kfree(t);
 
@@ -240,5 +283,3 @@ module_exit(tmpa9xx_adc_exit);
 MODULE_AUTHOR("Michael Hunold <michael@mihu.de>");
 MODULE_DESCRIPTION("TMPA9xx ADC driver");
 MODULE_LICENSE("GPL v2");
-
-EXPORT_SYMBOL_GPL(tmpa9xx_adc_read);
