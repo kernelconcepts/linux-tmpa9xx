6c81475315883769f7ac2fa73ffbbb709faa6af9
Michael Hunold <michael@mihu.de>
2011-03-11 15:39:23 +0100

    add support for different i2c bus speeds on the two busses. use speed_khz_0 and speed_khz_1

--- drivers/i2c/busses/i2c-tmpa9xx.c
+++ drivers/i2c/busses/i2c-tmpa9xx.c
@@ -21,9 +21,13 @@
 #include <linux/irq.h>
 #include <linux/slab.h>
 
-static int speed_khz = 100;
-module_param(speed_khz, int, S_IRUGO | S_IWUSR);
-MODULE_PARM_DESC(speed_khz, "i2c bus speed in khz");
+static int speed_khz_0 = 100;
+module_param(speed_khz_0, int, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(speed_khz, "i2c bus 0 speed in khz");
+
+static int speed_khz_1 = 100;
+module_param(speed_khz_1, int, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(speed_khz, "i2c bus 1 speed in khz");
 
 #define CR1	(0x00)	/* control register 1 */
 #define DBR	(0x04)	/* data buffer register*/
@@ -407,7 +411,7 @@ static int __devinit tmpa9xx_i2c_probe(struct platform_device *pdev)
 		goto err7;
 	}
 
-	calculate_prescaler_timing(clk_get_rate(priv->pclk)/1000, speed_khz, &priv->p);
+	calculate_prescaler_timing(clk_get_rate(priv->pclk)/1000, pdev->id ? speed_khz_1 : speed_khz_0, &priv->p);
 	dev_dbg(&pdev->dev, "prs %d, n %d, freq %d\n", priv->p.prs, priv->p.n, priv->p.freq);
 
 	/* setup scalers to 100khz */
