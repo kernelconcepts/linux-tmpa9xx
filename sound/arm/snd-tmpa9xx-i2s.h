e3e05ff9bcaf67c6887ef92ef032eedf479c8f01
Michael Hunold <michael@mihu.de>
2011-03-21 14:18:21 +0100

    add GPL boilerplate, disable debug

--- sound/arm/snd-tmpa9xx-i2s.h
+++ sound/arm/snd-tmpa9xx-i2s.h
@@ -1,3 +1,15 @@
+/*
+ * TMPA9xx I2S driver
+ *
+ * Copyright (c) 2010 Toshiba
+ * Copyright (c) 2011 Michael Hunold (michael@mihu.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ */
+
 #ifndef __TMPA9XX_I2S_H__
 #define __TMPA9XX_I2S_H__
 
